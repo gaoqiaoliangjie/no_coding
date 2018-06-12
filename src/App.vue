d6b488078e2fa51d2feeb32ddaec7537917fcae7
onerou <1057674027@qq.com>
2018-06-12 18:27:43 +0800

    余懿航上传

--- src/App.vue
+++ src/App.vue
@@ -5,31 +5,42 @@
     <!-- 顶栏容器 -->
     <el-header>
 
-      <!-- <Nevigation/>    -->
     </el-header>
     <!-- 顶栏容器end -->
-    <!-- 侧边栏容器 -->
-    <el-aside>
+    <el-container>
+      <!-- 侧边栏容器 -->
+      <el-aside width="auto" class="leftBox">
+        <Nevigation/>
+      </el-aside>
+      <!-- 侧边栏容器end -->
+    <el-container>
+      <!-- 主要区域容器 -->
+      <el-main>
+        <router-view/>
+      </el-main>
+      <!-- 主要区域容器end -->
+      <!-- 底栏容器 -->
+      <el-footer>
+      </el-footer>
+      <!-- 底栏容器eng -->
+    </el-container>
+  </el-container>
 
-    </el-aside>
-    <!-- 侧边栏容器end -->
-    <!-- 主要区域容器 -->
-    <el-main>
-      <router-view/>
-    </el-main>
-    <!-- 主要区域容器eng -->
-    <!-- 底栏容器 -->
-    <el-footer>
-    </el-footer>
-    <!-- 底栏容器eng -->
+
   </el-container>
   <!-- 外层容器end -->
 </div>
 </template>
 
 <script>
+import Nevigation from './components/nevigation.vue'
+// window.on('resize', () => {
+
+// })
+  $(".leftBox").css("height",$(window).height())
 export default {
-  name: 'App'
+  name: 'App',
+  components: { Nevigation }
 }
 </script>
 
@@ -40,5 +51,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+body,html{
+height: 100%;
+width:100%;
+padding: 0;
+margin: 0;
+}
+/* .scroll{
+  height: 100%;
+} */
+.leftBox{
+  height: 100%;
 }
 </style>
--- src/components/nevigation.vue
+++ src/components/nevigation.vue
@@ -1,20 +1,64 @@
 <template lang="html">
-  <div id="title">
+  <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+    <el-button class="CollapseBtn" v-bind:class="isCollapse ? errorClass: activeClass " v-on:click="change(isCollapse)" v-model="isCollapse" circle></el-button>
+    <el-scrollbar class="scroll">
+    <el-submenu index="1">
+     <template slot="title">
+       <i class="el-icon-phone"></i>
+      <span slot="title">首页</span>
+     </template>
+      <el-menu-item index="1-1">选项1</el-menu-item>
+      <el-menu-item index="1-2">选项2</el-menu-item>
+      <el-menu-item index="1-3">选项3</el-menu-item>
+    </el-submenu>
+    <el-submenu index="3">
+     <template slot="title">
+       <i class="el-icon-tickets"></i>
+      <span slot="title">二级页面</span>
+     </template>
+      <el-menu-item index="3-1">选项1</el-menu-item>
+      <el-menu-item index="3-2">选项2</el-menu-item>
+      <el-menu-item index="3-3">选项3</el-menu-item>
+    </el-submenu>
+    <el-menu-item index="2">
+       <i class="el-icon-menu"></i>
+       <span slot="title">导航二</span>
+     </el-menu-item>
+    </el-scrollbar>
 
-  </div>
-
+  </el-menu>
 </template>
 
 <script>
 export default {
-  name:'Nevigation',
-  data(){
-    return{
-      con:'<p>这是一段文字</p>'
+  name: 'Nevigation',
+  data() {
+    return {
+      isCollapse: false,
+      activeClass: "el-icon-d-arrow-left",
+      errorClass: "el-icon-d-arrow-right",
+    }
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      // console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      // console.log(key, keyPath);
+    },
+    change(isCollapse) {
+      this.isCollapse = !isCollapse
     }
   }
 }
 </script>
 
 <style lang="css">
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+  .scroll{
+    height: 100%;
+  }
 </style>
